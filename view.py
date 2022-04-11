f97a24308fdfeef956e0684459c6537879468a0b
Vyacheslav <dobrov.slava@yandex.ru>
2022-04-11 22:18:27 +0300

    Added changing for pull request

--- view.py
+++ view.py
@@ -3,6 +3,6 @@ def search():
     with open("Contacts.txt", 'r', encoding='utf-8') as f:
         for line in f:
             data.append([str(x) for x in line.split()])       
-    print(data[0])
+    print(data)
 
 search()
